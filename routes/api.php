a2a6e5ad844dbcc51a3e92568799fce8c51f68f6
DanielSlavulete <danielslavulete@gmail.com>
2026-03-25 15:24:09 +0100

    fix de peticiones por minuto

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,4 @@ use App\Http\Controllers\Api\SolicitudController;
 
 // Ruta real que recibe el formulario, ponemos que sea 3 solicitudes maximo por 1 dia (solicitudes que vengan de una misma IP, no globales)
 Route::post('/solicitudes', [SolicitudController::class, 'store'])
-    ->middleware(['wp.api.key', 'throttle:50,1']);
+    ->middleware(['wp.api.key', 'throttle:10,1']);
