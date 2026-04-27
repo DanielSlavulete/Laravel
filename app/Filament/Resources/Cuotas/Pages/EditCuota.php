02c802b7969d62832ab7516f83e9c868ba05293e
DanielSlavulete <danielslavulete@gmail.com>
2026-04-27 17:39:12 +0200

    Arreglo edit cuota

--- app/Filament/Resources/Cuotas/Pages/EditCuota.php
+++ app/Filament/Resources/Cuotas/Pages/EditCuota.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Cuotas\Pages;
 use App\Filament\Resources\Cuotas\CuotaResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\DB;
 
 class EditCuota extends EditRecord
 {
@@ -15,5 +16,33 @@ class EditCuota extends EditRecord
         return [
             DeleteAction::make(),
         ];
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if (! (bool) ($data['pagado'] ?? false)) {
+            $data['cuantia'] = null;
+            $data['fecha_pago'] = null;
+        }
+
+        return $data;
+    }
+
+    protected function handleRecordUpdate($record, array $data): \Illuminate\Database\Eloquent\Model
+    {
+        $pagado = (bool) ($data['pagado'] ?? false);
+
+        DB::table('cuotas')
+            ->where('id', $record->id)
+            ->update([
+                'socio_id' => $data['socio_id'],
+                'anio' => (int) $data['anio'],
+                'pagado' => DB::raw($pagado ? 'true' : 'false'),
+                'cuantia' => $pagado ? ($data['cuantia'] ?? null) : null,
+                'fecha_pago' => $pagado ? ($data['fecha_pago'] ?? null) : null,
+                'updated_at' => now(),
+            ]);
+
+        return $record->refresh();
     }
 }
